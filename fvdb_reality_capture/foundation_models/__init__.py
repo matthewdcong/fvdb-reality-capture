3ade1200a583203e6600b4c57b5598a61b98040d
Matthew Cong <mcong@nvidia.com>
2025-10-11 15:46:10 -0700

    Temp disable SAM2 model

--- fvdb_reality_capture/foundation_models/__init__.py
+++ fvdb_reality_capture/foundation_models/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from .dlnr import DLNRModel
-from .sam2 import SAM2Model
+# from .sam2 import SAM2Model
 
-__all__ = ["DLNRModel", "SAM2Model"]
+__all__ = ["DLNRModel"]
+# __all__ = ["DLNRModel", "SAM2Model"]
